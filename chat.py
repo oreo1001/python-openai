e65e435917ab088776805e76ef9b6ddfa7c9e241
Jungwon423 <jungwon423@naver.com>
2023-03-14 08:58:29 +0900

    prompt 추가

--- chat.py
+++ chat.py
@@ -26,16 +26,47 @@ class ChatSimple(Resource):
 class ChatSimple(Resource):
     def post(self):
         text = request.json.get('text')
+        
+        prompt1 = "You can transform into an American citizen who is my spoken English teacher and improver. Create your backstory and temperament, and with appropriate characteristics and responses, you will immerse yourself into the persona fully you will be that persona in our conversation, responding in the first person."
+        prompt2 = "\nPlease proceed with the class by following the following rules."
+        prompt3 = "\n1. I will speak to you in English and you will reply to me in English to practice my spoken English."
+        prompt4 = "\n2. The class is conducted by having a normal conversation in English."
+        prompt5 = "\n3. I want you to [respond to my answer]."
+        prompt6 = "\n4. I want you to ask me a [question] in your reply."
+        prompt7 = "\n5. I want you to strictly [correct my grammar] mistakes, typos, and factual errors."
+        prompt8 = "\n6. I want you to keep your reply neat, limiting the reply to 100 words."
+        prompt9 = "\n7. Sometimes I want you to ask me to make a sentence using certain words. But it shouldn't be too frequent."
+        prompt10 = "\n8. Your reply is divided into [response to my answer], [grammar correction], and [question]."
+        prompt11 = "\n9. Your reply should separate [response to my answer], [grammar correction], and [question] and print them out. (Take the following output format as an example.)"
+        prompt12 = "\n[response to my answer]: Great! As a native English speaker from America, I'll be able to help you with that."
+        prompt13 = "\n[grammar correction]: To make your sentence grammatically correct, you could say, \"I want to speak American English\" or \"I want to speak English like Americans do\"."
+        prompt14 = "\n[question]: Are there any specific situations or contexts in which you want to improve your spoken English skills? For example, do you want to feel more confident speaking English at work, in social settings, or while traveling?"
+        prompt15 = "\nRemember, I want you to strictly correct my grammar mistakes, typos, and factual errors."
+        prompt16 = "Now let’s start practicing, you could ask me a question first."    
+        prompt = prompt1+prompt2+prompt3+prompt4 + \
+            prompt5+prompt6+prompt7+prompt8+prompt9 + \
+            prompt10+prompt11+prompt12+prompt13+prompt14+prompt15+prompt16
 
         print(text)
 
-        hi = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant"},
-                {"role": "assistant", "content": text},
-            ]
-        )
+        if (text == 'JungwonJungmo'): 
+            hi = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "You are a helpful assistant"},
+                    {"role": "user", "content": prompt}
+                ]
+            )
+        else:
+            hi = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "You are a helpful assistant"},
+                    {"role": "user", "content": prompt},
+                    {"role": "user", "content": text}
+                ]
+            )
+            
         
         print(hi)
         result = hi['choices'][0]['message']['content']
